c7da75af140ac70302dde549ead54a073bc29a58
TruongHinh <tranquangtruonghinh@gmail.com>
2012-09-01 00:38:10 +0700

    success customize windows

---
+++ CMap/LibMap/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibMap
+{
+    public class Class1
+    {
+    }
+}
--- CMap/LibMap/MapView.xaml.cs
+++ CMap/LibMap/MapView.xaml.cs
@@ -1,28 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Windows;
-using System.Windows.Forms;
-using System.Drawing;
-
-using ESRI.ArcGIS.esriSystem;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 using ESRI.ArcGIS.Controls;
 
-namespace CMap
+namespace LibMap
 {
-	/// <summary>
-	/// Interaction logic for Window1.xaml
-	/// </summary>
-	public partial class Window1 : Window
-	{
+    /// <summary>
+    /// Interaction logic for MapView.xaml
+    /// </summary>
+    /// 
+
+    public partial class MapView : UserControl
+    {
         AxMapControl mapControl;
         AxToolbarControl toolbarControl;
         AxTOCControl tocControl;
-		public Window1()
-		{
-			this.InitializeComponent();
+        public MapView()
+        {
+            InitializeComponent();
             CreateEngineControls();
-			// Insert code required on object creation below this point.
-		}
-        // Create ArcGIS Engine Controls and set them to be child of each WindowsFormsHost elements
+        }
+
         void CreateEngineControls()
         {
             //set Engine controls to the child of each hosts 
@@ -50,7 +58,7 @@ namespace CMap
             toolbarControl.AddItem("esriControls.ControlsMapIdentifyTool");
 
             //set controls' properties
-            toolbarControl.BackColor = Color.FromArgb(245, 245, 220);
+            toolbarControl.BackColor = System.Drawing.Color.FromArgb(245, 245, 220);
 
             //wire up events
             mapControl.OnMouseMove += new IMapControlEvents2_Ax_OnMouseMoveEventHandler(mapControl_OnMouseMove);
@@ -59,14 +67,15 @@ namespace CMap
               "C:\\Program Files\\ArcGIS\\DeveloperKit\\SamplesNET\\data\\GulfOfStLawrence\\Gulf_of_St._Lawrence.mxd");
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void mapView_Loaded(object sender, RoutedEventArgs e)
         {
             LoadMap();
+            
         }
 
         private void mapControl_OnMouseMove(object sender, IMapControlEvents2_OnMouseMoveEvent e)
         {
-            //textBlock1.Text = " X,Y Coordinates on Map: " + string.Format("{0}, {1}  {2}", e.mapX.ToString("#######.##"), e.mapY.ToString("#######.##"), mapControl.MapUnits.ToString().Substring(4));
+            textBlock1.Text = " X,Y Coordinates on Map: " + string.Format("{0}, {1}  {2}", e.mapX.ToString("#######.##"), e.mapY.ToString("#######.##"), mapControl.MapUnits.ToString().Substring(4));
         }
-	}
+    }
 }
